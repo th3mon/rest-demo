502cdfd5fa49e425103d5cd629690eb73c57c9db
Przemysław Szelenberger <p.szelenberger@gmail.com>
2017-08-29 22:46:02 +0200

    Users API.

--- app.js
+++ app.js
@@ -5,11 +5,25 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var mysql = require('mysql');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// Database connection
+app.use((req, res, next) => {
+  res.locals.connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rest@PI01',
+    database: 'test'
+  });
+
+  res.locals.connection.connect();
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,7 +44,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/api/v1/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  res.locals.connection.query('SELECT * from users', (error, results) => {
+    if (error) {
+      res.send(JSON.stringify({
+        'status': 500,
+        'error': error,
+        'response': null
+      }));
+
+      throw error;
+    }
+    else {
+      res.send(JSON.stringify({
+        'status': 200,
+        'error': null,
+        'response': results
+      }));
+    }
+  });
 });
 
 module.exports = router;
